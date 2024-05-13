07839dd5e9f67f153699d1de1d6de0049c285f8f
Paku Thao <thao2210@stthomas.edu>
2024-05-12 19:49:20 -0500

    model update

--- src/renderer.js
+++ src/renderer.js
@@ -217,50 +217,34 @@ class Renderer {
 
         // Create point light sources
         //light 0
-        let light0 = new PointLight('light0', new Vector3(1.0, 1.0, 5.0), scene);
+        let light0 = new PointLight('light0', new Vector3(0.5, 3.0, 1.0), scene);
         light0.diffuse = new Color3(1.0, 1.0, 1.0);
         light0.specular = new Color3(1.0, 1.0, 1.0);
         current_scene.lights.push(light0);
 
         // light 1 
-        let light1 = new PointLight('light1', new Vector3(1.0, 1.0, 5.0), scene);
+        let light1 = new PointLight('light1', new Vector3(2.0, 3.0, -0.5), scene);
         light1.diffuse = new Color3(0.5, 0.5, 0.5);
         light1.specular = new Color3(0.5, 0.5, 0.5);
         current_scene.lights.push(light1);
 
         // light 2
-        let light2 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
+        let light2 = new PointLight('light1', new Vector3(0.5, 3.0, -2.0), scene);
         light2.diffuse = new Color3(0.5, 0.5, 0.5);
         light2.specular = new Color3(0.5, 0.5, 0.5);
         current_scene.lights.push(light2);
 
         // light 3
-        let light3 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
+        let light3 = new PointLight('light1', new Vector3(-1.0, 3.0, -0.5), scene);
         light3.diffuse = new Color3(0.5, 0.5, 0.5);
         light3.specular = new Color3(0.5, 0.5, 0.5);
         current_scene.lights.push(light3);
-
-        window.addEventListener("keypress", a => {
-            if (String.fromCharCode(a.keyCode) === 'a') {
-                current_scene.lights[this.active_light].position.x -= 1;
-            } else if (String.fromCharCode(a.keyCode) === 'd') {
-                current_scene.lights[this.active_light].position.x += 1;
-            } else if (String.fromCharCode(a.keyCode) === 'f') {
-                current_scene.lights[this.active_light].position.y -= 1;
-            } else if (String.fromCharCode(a.keyCode) === 'r') {
-                current_scene.lights[this.active_light].position.y += 1;
-            } else if (String.fromCharCode(a.keyCode) === 'w') {
-                current_scene.lights[this.active_light].position.z -= 1;
-            } else if (String.fromCharCode(a.keyCode) === 's') {
-                current_scene.lights[this.active_light].position.z += 1;
-            }
-            console.log(current_scene.lights[this.active_light].position);
-        });
 
         // Create ground mesh
         let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
         let rocky_texture = new Texture(BASE_URL + 'texturemaps/canyon_rgb.jpg', scene);
         let ground_heightmap = new Texture(BASE_URL + 'heightmaps/canyon_elev.jpg', scene);
+        let brick_texture = new Texture(BASE_URL + 'texturemaps/brick_wall.jpg', scene); 
         ground_mesh.scaling = new Vector3(30.0, 1.0, 15.0);
         ground_mesh.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
@@ -281,55 +265,96 @@ class Renderer {
             0.000, 0.000, 0.000, //0
             0.000, 0.000, -1.000, //1
             1.000, 0.000, -1.000, //2
-            1.000, 0.000, 0.000, //3
-            0.000, 1.000, 0.000, //4
-            0.000, 1.000, -1.000, //5
-            1.000, 1.000, -1.000, //6
-            1.000, 1.000, 0.000, //7
-            0.500, 1.500, 0.000, //8
-            0.500, 1.500, -1.000, //9
-            0.650, 1.000, -0.200, //10 start of chimney, bottom points
-            0.650, 1.000, -0.400, //11
-            0.850, 1.000, -0.400, //12
-            0.850, 1.000, -0.200, //13 
-            0.650, 1.500, -0.200, //14 top points
-            0.650, 1.500, -0.400, //15
-            0.850, 1.500, -0.400, //16
-            0.850, 1.500, -0.200, //17
+            1.000, 0.000, 0.000, //3 bottom of house
+            0.000, 0.000, 0.000, //4
+            0.000, 0.000, -1.000, //5
+            0.000, 1.000, 0.000, //6
+            0.000, 1.000, -1.000, //7 left wall
+            1.000, 0.000, -1.000, //8
+            0.000, 0.000, -1.000, //9
+            0.000, 1.000, -1.000, //10
+            1.000, 1.000, -1.000, //11 back wall
+            1.000, 0.000, -1.000, //12
+            1.000, 1.000, -1.000, //13
+            1.000, 1.000, 0.000, //14
+            1.000, 0.000, 0.000, //15 left wall
+            0.000, 0.000, 0.000, //16
+            0.000, 1.000, 0.000, //17
+            1.000, 1.000, 0.000, //18
+            1.000, 0.000, 0.000, //19 front wall
+            0.650, 1.000, -0.200, //20
+            0.850, 1.000, -0.200, //21
+            0.650, 1.500, -0.200, //22
+            0.850, 1.500, -0.200, //23 front chimney wall
+            0.650, 1.000, -0.200, //24
+            0.650, 1.000, -0.400, //25
+            0.650, 1.500, -0.200, //26
+            0.650, 1.500, -0.400, //27 left chimney wall
+            0.650, 1.000, -0.400, //28
+            0.850, 1.000, -0.400, //29
+            0.650, 1.500, -0.400, //30
+            0.850, 1.500, -0.400, //31 back chimney wall
+            0.850, 1.000, -0.400, //32
+            0.850, 1.000, -0.200, //33
+            0.850, 1.500, -0.400, //34
+            0.850, 1.500, -0.200, //35 right chimney wall
         ];
         let house_indices = [
             0, 1, 2,
             0, 2, 3, //bottom of house
-            0, 4, 5,
-            0, 5, 1, //left wall
-            1, 5, 6,
-            1, 6, 2, // back wall
-            2, 6, 7,
-            2, 7, 3, //right wall
-            0, 4, 7,
-            0, 7, 3, //front wall
-            4, 8, 7, //front roof
-            4, 8, 9,
-            4, 9, 5, //left roof
-            5, 9, 6, //back roof
-            6, 9, 8, 
-            6, 8, 7, //right root
-            10, 14, 15,
-            10, 15, 11,
-            15, 16, 12,
-            15, 12, 11, 
-            16, 17, 13, 
-            16, 13, 12,
-            17, 15, 10,
-            17, 10, 13
+            6, 5, 4,
+            6, 7, 5, //left wall
+            11, 8, 9,
+            11, 9, 10, //back wall
+            12, 13, 14, 
+            12, 14, 15, //left wall
+            18, 17, 16,
+            18, 16, 19, //front wall
+            20, 22, 23,
+            20, 23, 21, //front chimney wall
+            24, 25, 26,
+            26, 25, 27, //left chimney wall
+            28, 30, 31,
+            28, 31, 29, //back chimney wall
+            32, 33, 34,
+            34, 33, 35, //right chimney wall
         ];
         let vertex_normals = [
-            0.0, 0.0, 1.0,
-            0.0, 0.0, 1.0,
-            0.0, 0.0, 1.0,
+            0.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            -1.0, 0.0, 0.0,
+            -1.0, 0.0, 0.0,
             0.0, 0.0, -1.0,
             0.0, 0.0, -1.0,
-            0.0, 0.0, -1.0
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            -1.0, 0.0, 0.0,
+            -1.0, 0.0, 0.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
         ];
 
         let vertex_data = new VertexData();
@@ -349,6 +374,68 @@ class Renderer {
         house.material = materials['illum_' + this.shading_alg];
         
         current_scene.models.push(house);
+
+        //roof
+        let roof = new Mesh('roof', scene);
+        let roof_vertex_positions = [
+            0.000, 1.000, 0.000, //0
+            1.000, 1.000, 0.000, //1
+            0.500, 1.500, 0.000, //2 front roof
+            0.000, 1.000, 0.000, //3
+            0.500, 1.500, 0.000, //4
+            0.000, 1.000, -1.000, //5
+            0.500, 1.500, -1.000, //6 right roof
+            0.000, 1.000, -1.000, //7
+            1.000, 1.000, -1.000, //8
+            0.500, 1.500, -1.000, //9 back roof
+            1.000, 1.000, 0.000, //10
+            0.500, 1.500, 0.000, //11
+            1.000, 1.000, -1.000, //12
+            0.500, 1.500, -1.000, //13 left roof
+        ];
+        let roof_indices = [
+            0, 1, 2, //front roof
+            3, 4, 6,
+            3, 6, 5, //right roof
+            7, 8, 9, //back roof
+            10, 11, 13,
+            10, 13, 12, //left roof
+
+        ];
+        let roof_vertex_normals = [
+           0.0, 0.0, 1.0,
+           -1.0, 0.0, 0.0,
+           -1.0, 0.0, 0.0,
+           -1.0, 0.0, 0.0,
+           -1.0, 0.0, 0.0,
+           0.0, 0.0, -1.0,
+           0.0, 0.0, -1.0,
+           0.0, 0.0, -1.0,
+           0.0, 0.0, -1.0,
+           0.0, 0.0, -1.0,
+           1.0, 0.0, 0.0,
+           1.0, 0.0, 0.0,
+           1.0, 0.0, 0.0,
+           1.0, 0.0, 0.0,
+        ];
+
+        let roof_vertex_data = new VertexData();
+        roof_vertex_data.positions = roof_vertex_positions;
+        roof_vertex_data.indices = roof_indices;
+        roof_vertex_data.normals = roof_vertex_normals;
+        roof_vertex_data.applyToMesh(roof);
+
+        // Assign triangle a material and set its transforms
+        roof.metadata = {
+            mat_color: new Color3(0.75, 0.15, 0.05),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        roof.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(roof);
 
         // Animation function - called before each frame gets rendered
         scene.onBeforeRenderObservable.add(() => {
@@ -389,17 +476,24 @@ class Renderer {
         current_scene.lights.push(light0);
 
         // light 1 
-        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
+        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, -3.0), scene);
         light1.diffuse = new Color3(0.5, 0.5, 0.5);
         light1.specular = new Color3(0.5, 0.5, 0.5);
         current_scene.lights.push(light1);
+
+        // light 1 
+        let light2 = new PointLight('light1', new Vector3(1.0, 3.0, 6.0), scene);
+        light2.diffuse = new Color3(0.5, 0.5, 0.5);
+        light2.specular = new Color3(0.5, 0.5, 0.5);
+        current_scene.lights.push(light2);
+
 
         // Create ground mesh
         let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
         let ground_heightmap = new Texture(BASE_URL + 'heightmaps/default.png', scene);
         ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
         ground_mesh.metadata = {
-            mat_color: new Color3(0.40, 0.45, 0.15),
+            mat_color: new Color3(0.10, 0.65, 0.15),
             mat_texture: white_texture,
             mat_specular: new Color3(0.0, 0.0, 0.0),
             mat_shininess: 1,
@@ -434,6 +528,10 @@ class Renderer {
            -0.951, 0.309, 1.000, //20
 	        -0.224, 0.309, 1.000, //21
         ];
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3 + 1] += 6;
+        }
         let star_indices = [
             0, 1, 2, // back top point
             1, 3, 4, //top right point
@@ -473,14 +571,43 @@ class Renderer {
             0, 21, 10
         ];
 
+        let vertex_normals = [
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, -1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+        ];
+
         let vertex_data = new VertexData();
         vertex_data.positions = vertex_positions;
         vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
         vertex_data.applyToMesh(star);
 
         // Assign triangle a material and set its transforms
         star.metadata = {
-            mat_color: new Color3(0.75, 0.15, 0.05),
+            mat_color: new Color3(0.40, 0.40, 0.00),
             mat_texture: white_texture,
             mat_specular: new Color3(0.4, 0.4, 0.4),
             mat_shininess: 4,
@@ -489,6 +616,125 @@ class Renderer {
         star.material = materials['illum_' + this.shading_alg];
         
         current_scene.models.push(star);
+
+        let star2 = new Mesh('star2', scene);
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3] += 5;
+        }
+
+        vertex_data = new VertexData();
+        vertex_data.positions = vertex_positions;
+        vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
+        vertex_data.applyToMesh(star2);
+
+        star2.metadata = {
+            mat_color: new Color3(0.40, 0.40, 0.00),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        star2.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(star2);
+
+
+        star2 = new Mesh('star2', scene);
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3 + 1] -= 3;
+        }
+
+        vertex_data = new VertexData();
+        vertex_data.positions = vertex_positions;
+        vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
+        vertex_data.applyToMesh(star2);
+
+        star2.metadata = {
+            mat_color: new Color3(0.40, 0.40, 0.00),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        star2.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(star2);
+
+
+        star2 = new Mesh('star2', scene);
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3] -= 5;
+        }
+
+        vertex_data = new VertexData();
+        vertex_data.positions = vertex_positions;
+        vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
+        vertex_data.applyToMesh(star2);
+
+        star2.metadata = {
+            mat_color: new Color3(0.40, 0.40, 0.00),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        star2.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(star2);
+
+        star2 = new Mesh('star2', scene);
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3 + 1] -= 1;
+            vertex_positions[i * 3] += 1.5;
+        }
+
+        vertex_data = new VertexData();
+        vertex_data.positions = vertex_positions;
+        vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
+        vertex_data.applyToMesh(star2);
+
+        star2.metadata = {
+            mat_color: new Color3(0.40, 0.40, 0.00),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        star2.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(star2);
+
+        star2 = new Mesh('star2', scene);
+
+        for (let i = 0; i < vertex_positions.length / 3; i++) {
+            vertex_positions[i * 3] += 2;
+        }
+
+        vertex_data = new VertexData();
+        vertex_data.positions = vertex_positions;
+        vertex_data.indices = star_indices;
+        vertex_data.normals = vertex_normals;
+        vertex_data.applyToMesh(star2);
+
+        star2.metadata = {
+            mat_color: new Color3(0.40, 0.40, 0.00),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        star2.material = materials['illum_' + this.shading_alg];
+        
+        current_scene.models.push(star2);
+
 
         // Animation function - called before each frame gets rendered
         scene.onBeforeRenderObservable.add(() => {
